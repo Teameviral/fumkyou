885add8aa8acd106dff550cae9cc8028e595f611
Aditya <70588365+Aditya-XD@users.noreply.github.com>
2021-12-07 15:34:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/LEGENDXOP/sessionhack_bot")
-os.system("cd sessionhack_bot && python bot.py")
+os.system("git clone https://github.com/Aditya-XD/fumkyou")
+os.system("cd fumkyou && python bot.py")
